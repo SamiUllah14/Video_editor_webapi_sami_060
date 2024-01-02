2b5ca7fc80c577eca4f3a51e49cfa597cacb2839
Sami Afzal <egtsami@gmail.com>
2024-01-02 18:58:26 +0500

    Add project files.

---
+++ Controllers/VideoController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Video_editor_webapi_sami_060.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VideoController : ControllerBase
+    {
+        [HttpGet]
+        public string SayHelloAPI()
+        {
+            return "Hello To API";
+        }
+    }
+}
